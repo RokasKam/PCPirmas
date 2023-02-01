385565c9bae7f90883c99e073ff238920b3a9100
Rokas Kamblevicius <rokaskamble39@gmail.com>
2023-02-01 19:06:46 +0200

    push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { ageCalculator } from '../services/ageCalculator';
 
 function Home() {
     const [date, setDate] = useState(new Date());
-    const [age, setAge] = useState();
+    const [age, setAge] = useState({ageNow: 0, monthsNow: 0});
     const handleSubmit = (e) => {
       e.preventDefault();
       setAge(ageCalculator(date));
@@ -19,7 +19,8 @@ function Home() {
         onChange={(e) => setDate(e.target.value)}></input><br></br>
       <input type="submit" value="Submit"></input>
       </form> 
-      <p>Year:{age.ageNow}Month:{age.monthsNow}</p>
+      <p>Year:{age.ageNow}</p>
+      {process.env.NODE_ENV === 'production' && <p>Month:{age.monthsNow}</p>}
     </div>
     )
 }
